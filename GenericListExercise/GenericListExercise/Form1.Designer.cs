78f714a46a1280aeca27049a2a4e590f0ae1f4a0
Nikita Mieshalnykov <48445041+theonewithcurlyhair@users.noreply.github.com>
2019-04-26 11:29:28 -0300

    Finished the task

--- GenericListExercise/GenericListExercise/Form1.Designer.cs
+++ GenericListExercise/GenericListExercise/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(251, 117);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(80, 25);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score :";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(380, 117);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 31);
+            this.txtScore.TabIndex = 1;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(256, 200);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(224, 38);
+            this.btnFind.TabIndex = 2;
+            this.btnFind.Text = "Find Grade";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnFind;
     }
 }
 
--- GenericListExercise/GenericListExercise/Form1.cs
+++ GenericListExercise/GenericListExercise/Form1.cs
@@ -8,13 +8,74 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// Author: Nikita Mieshalnykov
+/// Project: GeneriCListExercise
+/// Date: April 26th, 2019
+/// </summary>
+
 namespace GenericListExercise
 {
+    public struct Grade
+    {
+        public int maxPoints;
+        public string mark;
+    }
+
     public partial class Form1 : Form
     {
+        private Grade grade;
+        private List<Grade> allGrades = new List<Grade>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGrades();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            FindingMark();
+        }
+        
+        //Loading grades to a generic list according to maximum and minimum points
+        private void LoadGrades()
+        {
+            grade.maxPoints = 299;
+            grade.mark = "F";
+            allGrades.Add(grade);
+
+            grade.maxPoints = 349;
+            grade.mark = "D";
+            allGrades.Add(grade);
+
+            grade.maxPoints = 399;
+            grade.mark = "C";
+            allGrades.Add(grade);
+
+            grade.maxPoints = 449;
+            grade.mark = "B";
+            allGrades.Add(grade);
+
+            grade.maxPoints = 500;
+            grade.mark = "A";
+            allGrades.Add(grade);
+        }
+
+        //iterating through each grade to find a mark according to the max points
+        private void FindingMark()
+        {
+            int points = Convert.ToInt32(txtScore.Text);
+
+            foreach (var grade in allGrades.Where(e => e.maxPoints > points))
+            {
+                MessageBox.Show(grade.mark);
+                return;
+            }
         }
     }
 }
